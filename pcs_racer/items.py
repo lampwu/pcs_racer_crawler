7bf5b0072c7a61898d04848930e145dfc863a8c7
lampwu <lamp7850053@gmail.com>
2019-06-11 22:13:14 +0800

    add bio detail

--- pcs_racer/items.py
+++ pcs_racer/items.py
@@ -8,13 +8,49 @@
 import scrapy
 
 
-class TutorialItem(scrapy.Item):
+class pcsRacerItem(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
-    raceName = scrapy.Field()
-    raceDate = scrapy.Field()
-    racePageLink = scrapy.Field()
-    flag = scrapy.Field()
-    raceClass = scrapy.Field()
-    raceWinner = scrapy.Field()
-    raceWinnerLink = scrapy.Field()
+    riderName = scrapy.Field()
+    teamName = scrapy.Field()
+    country = scrapy.Field()
+    riderCountryFlag = scrapy.Field()
+    countryLink = scrapy.Field()
+    riderDetails = scrapy.Field()
+    riderImg = scrapy.Field()
+    birth = scrapy.Field()
+    height = scrapy.Field()
+    wigtht = scrapy.Field()
+    placeOfBirth = scrapy.Field()
+    breakDown = scrapy.Field()
+    dataType = scrapy.Field()
+    ppsPoint = scrapy.Field()
+    socialNetLink = scrapy.Field()
+    pcsRankLink = scrapy.Field()
+    worldRankLink = scrapy.Field()
+    pcsRank = scrapy.Field()
+    uciRank = scrapy.Field()
+
+#bio
+
+        #riderName = response.css('div.content div.entry h1::text').get()
+        #teamName = response.css('div.content div.entry h1 span::text')[1].getall()
+        #riderCountryFlag = response.css('div.content div.entry span::attr(class)')[0].getall()
+        #riderDetails = response.css('.rdr-img-cont a::attr(href)').getall()
+        #riderImg = response.css('.rdr-img-cont a img::attr(src)').getall()
+        #birth = response.css('.rdr-info-cont::text').getall()
+        #countryLink = response.css('.rdr-info-cont a::attr(href)')[0].getall()
+        #country = response.css('.rdr-info-cont a::text')[0].getall()
+        #height = response.css('.rdr-info-cont span::text')[1].getall()
+        #wigtht = response.css('.rdr-info-cont span::text')[0].getall()
+        #placeOfBirth = response.css('.rdr-info-cont span span a::text')[0].getall()
+        #breakDown = response.css('.rdr-info-cont span span div a::attr(href)')[0].getall()
+        #dataType = response.css('.pps span div::attr(data-type)').getall()
+        #ppsPoint = response.css('.pps li  span::text').getall()
+        #socialNetLink = response.css('.rdr-info-cont span span div a::attr(href)').re(r'^http.+')
+        #pcsRankLink = response.css('.rdr-info-cont span span div a::attr(href)').re(r'^rank.+')[0]
+        #worldRankLink = response.css('.rdr-info-cont span span div a::attr(href)').re(r'^rank.+')[1]
+        #uciRankLink = response.css('.rdr-info-cont span span div a::attr(href)').re(r'^rank.+')[2]
+        #pcsRank =  response.css('.rdrStandings span::text')[0].getall()
+        #worldRank = response.css('.rdrStandings span::text')[1].getall()
+        #uciRank = response.css('.rdrStandings span::text')[2].getall()
--- pcs_racer/spiders/quotes_spider.py
+++ pcs_racer/spiders/quotes_spider.py
@@ -1,21 +1,76 @@
 import scrapy
-from tutorial.items import TutorialItem
+from pcs_racer.items import pcsRacerItem
 
 class PcsSpider(scrapy.Spider):
     name = "pcsrace"
     start_urls = [
-        'https://www.procyclingstats.com/races.php?year=2017&circuit=1&class=&filter=Filter',
-        'https://www.procyclingstats.com/races.php?year=2018&circuit=1&class=&filter=Filter'
+        'https://www.procyclingstats.com/rider/chad-haga'
     ]
 
     def parse(self, response):
         for race in response.css('tbody tr'):
-            item = TutorialItem() 
-            item['raceName'] = race.css("td a::text").get()
-            item['raceDate'] = race.css("td::text")[0].get()
-            item['racePageLink'] = race.css("td a::attr(href)").re(r'race.+')[0]
-            item['flag'] = race.css("td span::attr(class)").get()
-            item['raceClass'] = race.css("td::text")[2].get()
-            item['raceWinner'] = race.css("td.cu500 a::text").get()
-            item['raceWinnerLink'] = race.css("td.cu500 a::attr(href)")[0].get()
+            item = pcsRacerItem() 
+            item['riderName'] = response.css('div.content div.entry h1::text').get()
+            item['teamName'] = response.css('div.content div.entry h1 span::text')[1].getall()
+            item['country'] = response.css('div.content div.entry span::attr(class)')[0].getall()
+            item['riderCountryFlag'] = response.css('div.content div.entry span::attr(class)')[0].getall()
+            item['countryLink'] = response.css('.rdr-info-cont a::attr(href)')[0].getall()
+            item['riderDetails'] = response.css('.rdr-img-cont a::attr(href)').getall()
+            item['riderImg'] = response.css('.rdr-img-cont a img::attr(src)').getall()
+            item['birth'] = response.css('.rdr-info-cont::text').getall()
+            item['placeOfBirth'] = response.css('.rdr-info-cont span span a::text')[0].getall()
+            item['height'] = response.css('.rdr-info-cont span::text')[1].getall()
+            item['wigtht'] = response.css('.rdr-info-cont span::text')[0].getall()
+            item['breakDown'] = response.css('.rdr-info-cont span span div a::attr(href)')[0].getall()
+            item['dataType'] = response.css('.pps span div::attr(data-type)').getall()
+            item['ppsPoint'] = response.css('.pps li  span::text').getall()
+            item['socialNetLink'] = response.css('.rdr-info-cont span span div a::attr(href)').re(r'^http.+')
+            item['pcsRankLink'] = response.css('.rdr-info-cont span span div a::attr(href)').re(r'^rank.+')[0]
+            item['worldRankLink'] = response.css('.rdr-info-cont span span div a::attr(href)').re(r'^rank.+')[1]
+            item['pcsRank'] = response.css('.rdrStandings span::text')[0].getall()
+            item['uciRank'] = response.css('.rdrStandings span::text')[2].getall()
             yield item
+
+        '''riderName = scrapy.Field()
+    teamName = scrapy.Field()
+    country = scrapy.Field()
+    riderCountryFlag = scrapy.Field()
+    countryLink = scrapy.Field()
+    riderDetails = scrapy.Field()
+    riderImg = scrapy.Field()
+    birth = scrapy.Field()
+    height = scrapy.Field()
+    wigtht = scrapy.Field()
+    placeOfBirth = scrapy.Field()
+    breakDown = scrapy.Field()
+    dataType = scrapy.Field()
+    ppsPoint = scrapy.Field()
+    socialNetLink = scrapy.Field()
+    pcsRankLink = scrapy.Field()
+    worldRankLink = scrapy.Field()
+    pcsRank = scrapy.Field()
+    uciRank = scrapy.Field()'''
+
+        #bio
+
+        #riderName = response.css('div.content div.entry h1::text').get()
+        #teamName = response.css('div.content div.entry h1 span::text')[1].getall()
+        #riderCountryFlag = response.css('div.content div.entry span::attr(class)')[0].getall()
+        #riderDetails = response.css('.rdr-img-cont a::attr(href)').getall()
+        #riderImg = response.css('.rdr-img-cont a img::attr(src)').getall()
+        #birth = response.css('.rdr-info-cont::text').getall()
+        #countryLink = response.css('.rdr-info-cont a::attr(href)')[0].getall()
+        #country = response.css('.rdr-info-cont a::text')[0].getall()
+        #height = response.css('.rdr-info-cont span::text')[1].getall()
+        #wigtht = response.css('.rdr-info-cont span::text')[0].getall()
+        #placeOfBirth = response.css('.rdr-info-cont span span a::text')[0].getall()
+        #breakDown = response.css('.rdr-info-cont span span div a::attr(href)')[0].getall()
+        #dataType = response.css('.pps span div::attr(data-type)').getall()
+        #ppsPoint = response.css('.pps li  span::text').getall()
+        #socialNetLink = response.css('.rdr-info-cont span span div a::attr(href)').re(r'^http.+')
+        #pcsRankLink = response.css('.rdr-info-cont span span div a::attr(href)').re(r'^rank.+')[0]
+        #worldRankLink = response.css('.rdr-info-cont span span div a::attr(href)').re(r'^rank.+')[1]
+        #uciRankLink = response.css('.rdr-info-cont span span div a::attr(href)').re(r'^rank.+')[2]
+        #pcsRank =  response.css('.rdrStandings span::text')[0].getall()
+        #worldRank = response.css('.rdrStandings span::text')[1].getall()
+        #uciRank = response.css('.rdrStandings span::text')[2].getall()
