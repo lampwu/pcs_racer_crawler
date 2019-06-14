39890e8ada17d39fbb3ff225dc47b427385a7ae7
lampwu <lamp7850053@gmail.com>
2019-06-15 00:00:20 +0800

    add result head shirt's name/link/ranger

--- pcs_racer/spiders/racer_spider.py
+++ pcs_racer/spiders/racer_spider.py
@@ -23,7 +23,7 @@ class PcsSpider(scrapy.Spider):
             item['wigtht'] = racer.css('.rdr-info-cont span::text')[0].get()
             item['breakDown'] = racer.css('.rdr-info-cont span span div a::attr(href)')[0].get()
             item['dataType'] = racer.css('.pps span div::attr(data-type)').get()
-            item['ppsPoint'] = racer.css('.pps li  span::text').get()
+            item['ppsPoint'] = racer.css('.pps li  span::text').getall()
             item['socialNetLink'] = racer.css('.rdr-info-cont span span div a::attr(href)').re(r'^http.+')
             item['pcsRankLink'] = racer.css('.rdr-info-cont span span div a::attr(href)').re(r'^rank.+')[0]
             item['worldRankLink'] = racer.css('.rdr-info-cont span span div a::attr(href)').re(r'^rank.+')[1]
@@ -74,3 +74,53 @@ class PcsSpider(scrapy.Spider):
         #pcsRank =  response.css('.rdrStandings span::text')[0].getall()
         #worldRank = response.css('.rdrStandings span::text')[1].getall()
         #uciRank = response.css('.rdrStandings span::text')[2].getall()
+        
+        
+#Top result
+    #stageOrGc = response.css('ul.moblist div span.blue::text').get()
+    #topResultRace = response.css('ul.moblist div a::text').getall()
+    #raceType = response.css('ul.moblist div span.blue::text').getall()
+    #raceRank = response.css('ul.moblist div::text').getall()
+    #yearOfRace = response.css('ul.moblist div span::text').re(r'(\'\d.)')
+#Teams
+    #team = response.css('div.div2 ul li span a::text').getall()
+    #yearOfTeam = response.css('div.div2 ul li span::text').re(r'\d{4}')
+#Key statistics
+    #keystatistics = response.css('div.div2 ul.key-stats li div a::text').getall()
+    #keyNumber =  response.css('div.div2 ul.key-stats li div::text').getall()
+    #keyword = response.css('div.div2 ul.key-stats li div span::text').getall()
+#PCS Ranking position per season
+    #season = response.css('div.div4 ul.ranking-per-season li div.season a::text').getall()
+    #rps-points = response.css('div.div4 ul.ranking-per-season li div.bar div::text').getall()
+    #rps-pos = response.css('div.div4 ul.ranking-per-season li div.pos::text').getall()
+#Results per season
+    #season = response.css('div.div3 ul.horiztree li a::text').getall()
+    #seasonLink = response.css('div.div3 a.seasonResults::attr(href)').getall()
+#prresHead
+    #prresHead = response.css('div.results ul.prresHead li::text').getall()
+#prres = response.css('ul.prres')
+    #prresDate = prres.css('li div::text')[0].get()
+    #prresEndDate = prres.css('li div span::text')[0].get()
+    #prresFlag = prres.css('li div span.flag').getall()
+    #prresLink = prres.css('li div a::attr(href)')[0].getall()
+    #prresName = prres.css('li div a b::text')[0].get()
+    #prresLevel = prres.css('li div a::text')[0].getall()
+    
+    #First
+    #prresRangeFirst = prres.css('li ul li div::text')[0].get()
+    #prresShirtFirst = prres.css('li ul li div span::attr(class)')[0].get()
+    #prresShirtNameLinkFirst = prres.css('li ul li div a::attr(href)')[0].get()
+    #prresShirtNameFirst = prres.css('li ul li div a::text')[0].get()
+
+    #Second
+    #prresRangeSecond = prres.css('li ul li div::text')[1].get()
+    #prresShirtSecond = prres.css('li ul li div span::attr(class)')[1].get()
+    #prresShirtNameLinkSecond = prres.css('li ul li div a::attr(href)')[1].get()
+    #prresShirtNameSecond = prres.css('li ul li div a::text')[1].get()
+
+    #Third
+    #prresRangeSecond = prres.css('li ul li div::text')[2].get()
+    #prresShirtSecond = prres.css('li ul li div span::attr(class)')[2].get()
+    #prresShirtNameLinkSecond = prres.css('li ul li div a::attr(href)')[2].get()
+    #prresShirtNameSecond = prres.css('li ul li div a::text')[2].get()
+
